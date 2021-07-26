11ce6637c8ec7dba2ef2362dff4c7c47f481d6d7
antonfsp <antonfsp@gmail.com>
2021-07-26 17:21:48 +0200

    Close to final version

--- Code/src/main.py
+++ Code/src/main.py
@@ -5,50 +5,50 @@ import time # Control running time
 # Own modules
 import main_full_cooperation, main_partial_cooperation, main_residual_cooperation, main_iterative_cooperation, main_no_cooperation
 
-#-----------
-# For instances with 5 agents
-#--------
+# #-----------
+# # For instances with 5 agents
+# #--------
 
-instance_list = []
-n = 5
-for string in ['low','high']:
-    for i in range(5):
-        instance_list.append('%d_%s_%d' %(n,string,i))
+# instance_list = []
+# n = 5
+# for string in ['low','high']:
+#     for i in range(5):
+#         instance_list.append('%d_%s_%d' %(n,string,i))
 
-# Data frame to store results
-agents_5_df = pd.DataFrame(columns = ['No_cooperation','Full','Full%','Partial', 'Partial%','Residual','Residual%'])
-agents_5_time_df = pd.DataFrame(columns = ['No_cooperation','Full','Partial','Residual'])
+# # Data frame to store results
+# agents_5_df = pd.DataFrame(columns = ['No_cooperation','Full','Full%','Partial', 'Partial%','Residual','Residual%'])
+# agents_5_time_df = pd.DataFrame(columns = ['No_cooperation','Full','Partial','Residual'])
 
-for instance in instance_list[0:1]:
-    time_1 = time.time()
-    main_no_cooperation.no_cooperation(instance)
-    time_end_no_coop = time.time()
-    full_payoff = main_full_cooperation.full_cooperation(instance)
-    time_end_full = time.time()
-    partial_payoff = main_partial_cooperation.partial_cooperation(instance)
-    time_end_partial = time.time() 
-    no_cooperation_payoff , residual_payoff = main_residual_cooperation.residual_cooperation(instance)
-    time_end_residual = time.time()
+# for instance in instance_list:
+#     time_1 = time.time()
+#     main_no_cooperation.no_cooperation(instance)
+#     time_end_no_coop = time.time()
+#     full_payoff = main_full_cooperation.full_cooperation(instance)
+#     time_end_full = time.time()
+#     partial_payoff = main_partial_cooperation.partial_cooperation(instance)
+#     time_end_partial = time.time() 
+#     no_cooperation_payoff , residual_payoff = main_residual_cooperation.residual_cooperation(instance)
+#     time_end_residual = time.time()
     
-    temp = [no_cooperation_payoff,full_payoff,(full_payoff-no_cooperation_payoff)/no_cooperation_payoff*100,partial_payoff,
-    (partial_payoff-no_cooperation_payoff)/no_cooperation_payoff*100,residual_payoff,(residual_payoff-no_cooperation_payoff)/no_cooperation_payoff*100]
-    temp = [round(x,2) for x in temp]
+#     temp = [no_cooperation_payoff,full_payoff,(full_payoff-no_cooperation_payoff)/no_cooperation_payoff*100,partial_payoff,
+#     (partial_payoff-no_cooperation_payoff)/no_cooperation_payoff*100,residual_payoff,(residual_payoff-no_cooperation_payoff)/no_cooperation_payoff*100]
+#     temp = [round(x,2) for x in temp]
 
-    agents_5_df.loc[instance] = temp
+#     agents_5_df.loc[instance] = temp
 
-    temp_time = [time_end_no_coop-time_1,time_end_full-time_end_no_coop,time_end_partial-time_end_full,time_end_residual-time_end_partial]
-    temp_time = [round(x,2) for x in temp_time]
+#     temp_time = [time_end_no_coop-time_1,time_end_full-time_end_no_coop,time_end_partial-time_end_full,time_end_residual-time_end_partial]
+#     temp_time = [round(x,2) for x in temp_time]
 
-    agents_5_time_df.loc[instance] = temp_time
+#     agents_5_time_df.loc[instance] = temp_time
 
 
-file = open("5_agents_payoffs.txt","w")
-file.write(agents_5_df.to_latex())
-file.close()
+# file = open("5_agents_payoffs.txt","w")
+# file.write(agents_5_df.to_latex())
+# file.close()
 
-file = open("5_agents_times.txt","w")
-file.write(agents_5_time_df.to_latex())
-file.close()
+# file = open("5_agents_times.txt","w")
+# file.write(agents_5_time_df.to_latex())
+# file.close()
 
 #------------------------------------------------------------------------
 # For instances with 2 agents
@@ -68,7 +68,8 @@ agents_2_df = pd.DataFrame(columns = ['No_cooperation','Full','Full%','Partial',
 # Data frame to store running times
 agents_2_time_df = pd.DataFrame(columns = ['No_cooperation','Full','Partial','Residual','Iterative'])
 
-for instance in instance_list[0:1]:
+for instance in instance_list:
+    print(instance)
     time_1 = time.time()
     main_no_cooperation.no_cooperation(instance)
     time_end_no_coop = time.time()
@@ -104,10 +105,12 @@ file.close()
 
 # ------------------- ANALYSIS ORDER RELEVANCE FOR ITERATIVE MODEL ---------------------------
 
+print('Order analysis ---------------------------------')
 # Data frame to compare iterative model with different orders
 iter_order_df = pd.DataFrame(columns = ['Order1_payoff','Order2_payoff','Dif%','Order1_it','Order2_it','Dif_it'])
 
-for instance in instance_list[0:1]:
+for instance in instance_list:
+    print(instance)
     print(instance)
     iterative_payoff1,it_for_eq1 = main_iterative_cooperation.iterative_cooperation(instance,[0,1])
     iterative_payoff2,it_for_eq2 = main_iterative_cooperation.iterative_cooperation(instance,[1,0])
